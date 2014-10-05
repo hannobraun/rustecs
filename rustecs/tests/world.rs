121214391742c6f16dfabfc70d1149bb30ac699f
Hanno Braun <mail@hannobraun.de>
2014-10-05 14:51:31 +0200

    Refactor

--- rustecs/tests/world.rs
+++ rustecs/tests/world.rs
@@ -7,6 +7,24 @@ extern crate rustecs;
 #[phase(plugin)] extern crate rustecs_macros;
 
 
+world! {
+	// Inline entity constructor. This is good for the general case, since it
+	// avoids the duplication of external entity constructors.
+	entity_constructor missile(x: f64, y: f64) -> (Position, Visual) {
+		(
+			Position(x, y),
+			RenderAsMissile,
+		)
+	}
+
+	// This specifies an entity constructor that uses an external function. Can
+	// be useful for debugging, since compiler errors inside generated code are
+	// not very useful. There's a lot of duplication between the declaration
+	// here and the external function though.
+	entity_constructor ship(score: u32) -> (Position, Visual, Score) = create_ship;
+}
+
+
 #[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
 pub struct Position(f64, f64);
 
@@ -27,23 +45,6 @@ fn create_ship(score: u32) -> (Position, Visual, Score) {
 	)
 }
 
-
-world! {
-	// Inline entity constructor. This is good for the general case, since it
-	// avoids the duplication of external entity constructors.
-	entity_constructor missile(x: f64, y: f64) -> (Position, Visual) {
-		(
-			Position(x, y),
-			RenderAsMissile,
-		)
-	}
-
-	// This specifies an entity constructor that uses an external function. Can
-	// be useful for debugging, since compiler errors inside generated code are
-	// not very useful. There's a lot of duplication between the declaration
-	// here and the external function though.
-	entity_constructor ship(score: u32) -> (Position, Visual, Score) = create_ship;
-}
 
 #[test]
 fn it_should_create_entities() {
